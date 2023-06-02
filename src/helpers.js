0526eb30af8bccf0695c252f71d3ea7197237086
Jeremy Sobczak <jeremy@jeremys-mbp.lan>
2023-06-02 15:30:25 -0400

    Fixed bug. Customer is now hungrier.

--- src/helpers.js
+++ src/helpers.js
@@ -7,7 +7,7 @@ const choice = (items) => {
 const remove = (items, item) => {
     const idx = items.indexOf(item);
     // if item not found idx will be -1; if so return defined
-    if (idx == -1) return undefined;
+    if (idx === -1) return undefined;
     // otherwise splice out item from items and return that value
     const val = items.splice(idx, 1);
     return val;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import foods from "./food";
+import foods from "./foods";
 import { choice, remove } from "./helpers";
 
 const randomFruit = choice(foods);
 console.log(`I’d like one ${randomFruit}, please.`);
-const removed = remove(randomFruit);
+let removed = remove(foods,randomFruit);
 console.log(`Here you go: ${removed}`);
 console.log(`Delicious! May I have another?`);
 console.log(`I'm sorry, we're all out. We have ${foods} left.`);
+const anotherFruit = choice(foods);
+console.log(`I'll have a ${anotherFruit}, please.`)
+removed = remove(foods,anotherFruit);
+console.log(`Here you go: ${removed}`);
+console.log(`Yum!`);
+console.log(`That's my last ${removed}, but let me know if you would like any of these: ${foods}`);
